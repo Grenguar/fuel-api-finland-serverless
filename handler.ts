55290b43a6582571f6aa9d149089da8404959a8d
Igor Soroka <igor@soroka.tech>
2020-11-02 19:16:10 +0200

    adding error catching

--- handler.ts
+++ handler.ts
@@ -4,32 +4,37 @@ import { LambdaResponse } from './src/model/lambdaResponse';
 const url = 'https://www.polttoaine.net';
 const fuelScraper = new FuelScraper(url);
 
-const getLocations = async (event: any, context: any, callback: any) => {
+const getLocations = async (_event: any, _context: any, _callback: any): Promise<LambdaResponse> => {
   try {
     const cityLocations = await fuelScraper.getLocationNames();
-    const response: LambdaResponse = {
+    return {
       statusCode: 200,
       body: JSON.stringify(cityLocations)
     };
-    callback(null, response);
   } catch (err) {
     console.error(err);
-    const response: LambdaResponse = {
+    return {
       statusCode: 200,
       body: JSON.stringify('Error occured')
     };
-    callback(null, response);
   }
 };
 
-const getLocationPrices = async (event: any, _context: any, callback: any) => {
-  const locationName = event.pathParameters.name;
-  const stationsForLocation = await fuelScraper.getGasStationsForLocation(locationName);
-  const response: LambdaResponse = {
-    statusCode: 200,
-    body: JSON.stringify(stationsForLocation)
-  };
-  callback(null, response);
+const getLocationPrices = async (event: any, _context: any, _callback: any): Promise<LambdaResponse> => {
+  try {
+    const locationName = event.pathParameters.name;
+    const stationsForLocation = await fuelScraper.getGasStationsForLocation(locationName);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(stationsForLocation)
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 200,
+      body: JSON.stringify('Error occured')
+    };
+  }
 };
 
 export { getLocations, getLocationPrices };
